e67138a86d71fd31578c9d74bdd2e4ff56eb6486
Nacho <nboujon7@gmail.com>
2026-08-27 09:23:03 -0300

    Agregar soporte automatico para SendGrid SMTP

--- server.js
+++ server.js
@@ -90,16 +90,31 @@ const sendEmail = async (to, subject, html) => {
         }
         
         const nodemailer = require('nodemailer');
-        const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: process.env.SMTP_USER,
-                pass: process.env.SMTP_PASS
-            },
-            connectionTimeout: 5000,
-            greetingTimeout: 5000,
-            socketTimeout: 5000
-        });
+        const isSendGrid = process.env.SMTP_USER === 'apikey';
+        const transporter = nodemailer.createTransport(
+            isSendGrid 
+            ? {
+                host: 'smtp.sendgrid.net',
+                port: 587,
+                auth: {
+                    user: 'apikey',
+                    pass: process.env.SMTP_PASS
+                },
+                connectionTimeout: 5000,
+                greetingTimeout: 5000,
+                socketTimeout: 5000
+            } 
+            : {
+                service: 'gmail',
+                auth: {
+                    user: process.env.SMTP_USER,
+                    pass: process.env.SMTP_PASS
+                },
+                connectionTimeout: 5000,
+                greetingTimeout: 5000,
+                socketTimeout: 5000
+            }
+        );
 
         const mailOptions = {
             from: process.env.SMTP_USER,
